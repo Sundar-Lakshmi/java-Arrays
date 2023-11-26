aa0bc5e21cef0cff03e41d5b2567589151507da1
Sundar-Lakshmi <sundaralakshmipremi24@gmail.com>
2023-11-26 12:30:19 +0530

    Add the changes

--- Arrays/untitled/src/SortArrays.java
+++ Arrays/untitled/src/SortArrays.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class SortArrays {
+import java.util.Arrays;
+
+public class SortArrays {
+    public static void main(String[] args){
+        int[] a = {3,5,6,8,1};
+        int j = -1, temp = 0;
+        for(int i =0; i<a.length;i++){
+            if(a[i]%2 == 0){
+                j++;
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
 }
--- Arrays/untitled/src/SortTwoArrays.java
+++ Arrays/untitled/src/SortTwoArrays.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class SortTwoArrays {
+import java.util.Arrays;
+
+public class SortTwoArrays {
+    public static void main(String[] args){
+        int[] a = {0,2,5,8};
+        int[] b = {1,6,7};
+        int temp = 0;
+        for(int i =0; i<a.length; i++){
+            if(a[i] > b[i]){
+                temp = a[i];
+                a[i] = b[i];
+                b[i] = temp;
+            }
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+    }
 }
